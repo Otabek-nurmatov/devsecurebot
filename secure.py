84553851e9ec77bd650c0d7a292935b098ce02cd
mirafzaldev <mirafzaaal2609@gmail.com>
2022-11-17 18:29:14 +0500

    mirafzal added

--- secure.py
+++ secure.py
@@ -20,8 +20,12 @@ async def contact_share(msg: types.Message):
 # So'kish xabarlarni o'chirish uchun bads ga qo'shimcha so'zlar yozing...
 @dp.message_handler(IsGroup(), content_types=types.ContentType.TEXT)
 async def bad_msg(msg: types.Message):
-    bads = ['pidaraz','chmo','lata','ypoд','aptinga', 'xaromi','onangni','opangni','Xaromi','haromi','xaromi','harom','xarom','chichiqo','osiroq','mordanga', 'pidoraz', 'suka', 'dalbayop', 'dalpayop','tvar','тварь', 'qanjiq', 'yiban', 'yban', 'chumo', 'cumo', 'сука',
-            'долбаеб', 'пидораз', 'пидараз','kot','Kot','kotmsan','itdan tarqagan','it bashara','chlen','otsosi','Otsosi','Maraz','Gandon','kotinga sqi','kotnga skey ','aminga','amnga','Aminga',' qotog bosh','Ypa','ypa','yabalnik','kotnga ske ']
+    bads = ['pidaraz','chmo','lata','ypoд','aptinga', 'xaromi','onangni','opangni','Xaromi','haromi','xaromi','harom','xarom','chichiqo','osiroq','mordanga',
+             'pidoraz', 'suka', 'dalbayop', 'dalpayop','tvar','тварь', 'qanjiq', 'yiban', 'yban', 'chumo', 'cumo', 'сука',
+            'долбаеб', 'пидораз', 'пидараз','kot','Kot','kotmsan','konchenni','Kochenni','itdan tarqagan','it bashara','chlen','otsosi','Otsosi','Maraz','Gandon',
+            'kotinga sqi','kotnga skey ','aminga','amnga','Aminga',' qotog bosh','Ypa','ypa','yabalnik','kotnga ske ',
+            'qotogimi yepsan','negrnki','jalap','Jalap','jala','Jala','soska','Soska','qanju','Qanju','sssuka ble','ble','Ble','Blya','blya','blin','Blin',]
+   #aka faqat togri chunaz atak unaqa sokindgan bola masmiz bunaqa sokinishlarni optni bollardan topib qoidik bz bulani yaxshi niyatda yozdik       
     for bad_word in bads:
         if bad_word in msg.text.lower():
             await msg.delete()
